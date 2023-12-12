d7482deb1b1530cca1aaf83c19e8aed1f12322b9
Vince <v.coccina@orange.fr>
2023-12-12 11:10:06 +0100

    bot finalisations

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ secret_key = os.getenv("SECRET_KEY_TEST")
 # Initialisation de l'API Binance
 client = Client(api_key=api_key, api_secret=secret_key, tld='com',testnet=True)
 
+symbol = "ETHUSDT"
+volume = 0.01
+no_of_safety_orders = 5
+proportion = 1
+profit_target = 1
+
 def get_balance():
     x = client.get_account()
     df = pd.DataFrame(x["balances"])
@@ -41,16 +47,6 @@ def market_order(symbol,volume,direction):
         )
         return order["fills"]
 
-# df1 = pd.DataFrame(columns=["price", "qty"])
-# df2 = pd.DataFrame(columns=["price", "qty"])
-
-# while True:
-#     x = market_order("BTCUSDT", 0.002, "buy")
-#     df2.loc[0, "price"] = float(x[0]["price"])
-#     df2.loc[0, "qty"] = float(x[0]["qty"])
-#     df1 = pd.concat([df1,df2], ignore_index=True)
-#     print(df1)
-
 def cal_dev_from_initial_price(symbol, df):
     curr_price = float(get_curr_value(symbol))
     init_price = float(df.price[0])
@@ -75,6 +71,56 @@ def cal_pct_profit(symbol, df):
 def sell_all(symbol, df):
     volume = float(df["qty"].sum())
     market_order(symbol, volume, "sell")
+
+
+def run():
+    while True:
+        df1 = pd.DataFrame(columns=["price", "qty"])
+        df2 = pd.DataFrame(columns=["price", "qty"])
+        x = market_order(symbol, volume, "buy")
+        df2.loc[0, "price"] = float(x[0]["price"])
+        df2.loc[0, "qty"] = float(x[0]["qty"])
+        df1 = pd.concat([df1,df2], ignore_index=True)
+
+        curr_no_of_safety_orders = 0
+        multiplied_volume = volume * 2
+        deviation = -1
+        next_price_level = -1
+
+        while True:
+            dev = cal_dev_from_initial_price(symbol, df1)
+            print(f"deviation {dev}")
+            if dev <= next_price_level*proportion:
+                if curr_no_of_safety_orders < no_of_safety_orders:
+                    try:
+                        x = market_order(symbol, multiplied_volume, "buy")
+                        df2.loc[0, "price"] = float(x[0]["price"])
+                        df2.loc[0, "qty"] = float(x[0]["qty"])
+                        df1 = pd.concat([df1,df2], ignore_index=True)
+                    except:
+                        pass
+
+                    multiplied_volume *= 2
+                    deviation *= 2
+                    next_price_level += deviation
+                    curr_no_of_safety_orders += 1
+
+            pct_profit = cal_pct_profit(symbol, df1)
+            print(f"pct_profit {pct_profit}")
+            print(df1)
+
+            if pct_profit >= profit_target:
+                sell_all(symbol, df1)
+                break
+
+
+run()
+
+
+
+
+
+
 
 
 
